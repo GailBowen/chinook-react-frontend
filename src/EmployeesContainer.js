3c72be7c8f8a06bad6fedfdcf4f6643fa4fc2c64
David Wilson <davidwil@posteo.de>
2020-12-30 21:41:35 +0000

    Display employees by letter

--- src/EmployeesContainer.js
+++ src/EmployeesContainer.js
@@ -3,6 +3,8 @@ import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import { Link } from 'react-router-dom';
 
+import generateLetters from './util/GenerateLetters';
+
 const GET_EMPLOYEES = gql`
 {
   getEmployees{
@@ -24,14 +26,26 @@ const EmployeesContainer = () => (
 );
 
 const Employees = (props) => {
-  const employees = props.employees.map((l,i) => (
-    <li key={i}><Link to={`/employee/${l.EmployeeId}`}>{l.Title} {l.FirstName} {l.LastName}</Link></li>));
+  const employees = props.employees;
+  const letters = generateLetters(employees.map((x) => x.LastName));
+
+  const indexedEmployees = letters.map((l) => {
+
+    const es = employees.filter((e) => e.LastName.startsWith(l)).map((e) =>
+      <div><Link to={`/employee/${e.EmployeeId}`}>{e.LastName}, {e.FirstName} ({e.Title})</Link></div>
+    );
+
+    return (
+      <>
+      <h2 id={`letter-${l}`}>{l}</h2>
+      {es}
+      </>);
+  });
 
   return(
     <>
-    <ul>
-      {employees}
-    </ul>
+    <h1>Employees</h1>
+    {indexedEmployees}
     </>
   );
 }
